05e8ddff65da522225260922851bcc051d9aea7a
Logesh <116418351+LPSMOVIES@users.noreply.github.com>
2022-12-01 19:40:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,16 +2,16 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 19010782))
-    API_HASH = os.environ.get("API_HASH", "830f2543690bcfb28d27a47d485c1ba4")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5869785250:AAHFFu3cMrVcyiUfY_1_tjpHagARRG_uT34")
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     PORT = os.environ.get("PORT", "8080")
     BOT_SESSION_NAME = os.environ.get("BOT_SESSION_NAME", "LinkSearchBot")
-    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "BQAugWXUCCHwNouC64r2bVlLlFEOvDyg2yJHyKZLHzunYWS5r85xmqUL-BPm3wtoxJqzgS8-LOfuQnsYHAEI6j0NQxpM2IGDC2IeasN1I-NiNt4mcr4sc6zLzUpKKQWtXF6B2_Kme_h0SDM0ClnBpwzywOEEU4Y36Dx7NIK2dGE14FTaC6ZHitmmAwEuZqTlIoM823D8YvWYyxCRdIh5DKb6QqTiF4Q59cznqSymYwGUvozca976PNZmXGTftisOqEcTeMzco_bPO0x1C7_yZTNwaVlG2i90nJAVwIBC2rdTGMYrc14vyoM6lgMAjhFIYAoAtTrd4k2K4iLvRC_GTG5heWtSfwA")
-    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -1001883053973))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME, "Team_HDTLinks_bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "LOGESH"))
-    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Logesh:Logesh2004@cluster0.szthwiu.mongodb.net/?retryWrites=true&w=majority")
+    USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", "")
+    CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    DATABASE_URL = os.environ.get("DATABASE_URL")
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     ABOUT_BOT_TEXT = """<b>This is Team HDT Link Search Bot.
     
