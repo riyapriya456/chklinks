5a993227a27d75b15cb7fba67d117460fc34a421
Logesh <116418351+LPSMOVIES@users.noreply.github.com>
2022-12-01 19:08:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
-cd /Mdisk-Search-Bot
+git clone https://github.com/LPSMOVIES/chklinks /chklinks
+cd /chklinks
 pip3 install -r requirements.txt
 echo "Starting Bot....GreyMatter_Bots"
 python3 main.py
